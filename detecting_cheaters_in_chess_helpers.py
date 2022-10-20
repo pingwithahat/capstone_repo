427295484020e3b1d4271950fa533bd3b202d02e
pingwithahat <etlowcock@gmail.com>
2022-10-20 17:56:32 +0100

    update before going home

--- detecting_cheaters_in_chess_helpers.py
+++ detecting_cheaters_in_chess_helpers.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import re
 
 def chess_checker_function(df):
     expected_schema = {
@@ -46,7 +47,7 @@ def chess_checker_function(df):
         else:
             return True
         
-        
+    print(f'For year: {df.Date[0].year}')    
     schema_check = schema_checker(df, expected_schema.keys())
     dtype_check = dtype_checker(df, expected_schema)
     return print(f'Expected columns present: {schema_check} \nExpected dtypes present: {dtype_check}')
@@ -90,10 +91,10 @@ def chess_nan_checker(df, list_of_cols=['BlackIsComp', 'WhiteIsComp']):
     assert all([col in df.columns for col in list_of_cols]), f'Passed dataframe does not contain {list_of_cols}'   
     
     # are there any nan values?
-    any_nan = all(
+    no_nan = all(
         df[[x for x in df.columns if x not in list_of_cols]].notna()
     ) # returns True if all values are not NaN
-    if any_nan == True:
+    if no_nan == True:
         return print(f"This dataframe has 0 NaN values in columns: {list(df[[x for x in df.columns if x not in list_of_cols]].columns)}")
         
     # how many?
@@ -116,3 +117,109 @@ def chess_nan_checker(df, list_of_cols=['BlackIsComp', 'WhiteIsComp']):
         summary += f'\nMore than 50% of the data is missing from: {big_nan_cols}'
 
     return print(summary)
+
+
+def keep_decisive_results(df):
+    temp_ = df.copy()
+    # Computer wins as white 
+    comp_w_w = ((temp_['Result']=='1-0')&(temp_['WhiteIsComp']==1))
+    # Computer wins as black
+    comp_b_w = ((temp_['Result']=='0-1')&(temp_['BlackIsComp']==1))
+    temp_ = temp_[
+        ((temp_['Result']=='1-0')&(temp_['WhiteIsComp']==1))|
+        ((temp_['Result']=='0-1')&(temp_['BlackIsComp']==1))]
+    return temp_
+    
+
+def drop_uneeded_cols(df=None, list_of_cols=None, what_cols=False):
+    '''
+    By default, this will drop:
+    'Event', 'Site', 'Date', 'Round', 'White', 'Black', 'BlackClock', 'FICSGamesDBGameNo', 'Time', 'WhiteClock',
+        'Result'
+        
+            
+    Assert that df is dataframe
+    
+    '''
+    
+    col_to_drop = [
+    'Event', 'Site', 'Date', 'Round', 'White', 'Black', 'BlackClock', 'FICSGamesDBGameNo', 'Time', 'WhiteClock',
+    'Result'
+    ]
+    
+    if what_cols==True:
+        return(print(col_to_drop))
+    elif list_of_cols is not None and df is not None:
+        temp_ = df.drop(columns=list_of_cols)
+        print(f'Dropped columns: {list_of_cols}\n')
+        return temp_
+    else:
+        assert isinstance(df, pd.DataFrame), "Passed df must be a df"
+        temp_ = df.drop(columns=col_to_drop)
+        print(f'Dropped columns: {col_to_drop}\n')
+        return temp_
+    
+
+def split_timeformat(df):
+    temp_ = df.copy()
+    temp_[['TimeControl_Base', 'TimeControl_Inc']]=[
+    (int(str.split(x, sep='+')[0]), int(str.split(x, sep='+')[1])) for x in temp_.TimeControl.values]
+    temp_ = temp_.drop(columns=['TimeControl'])
+    return temp_
+    
+    
+def clean_pickle(pickle_df):
+    chess_checker_function(pickle_df)
+    temp_ = change_comp_columns(pickle_df)
+    chess_nan_checker(temp_)
+    temp_ = keep_decisive_results(temp_)
+    print(f'Keeping decisive results...')
+    temp_ = drop_uneeded_cols(temp_)
+    temp_ = split_timeformat(temp_)
+    return temp_
+
+
+def clean_pickles(list_of_pickle_df):
+    list_of_cleaned_pickle_df = []
+    for pickle_df in list_of_pickle_df:
+        list_of_cleaned_pickle_df.append(clean_pickle(pickle_df))
+        
+    return list_of_cleaned_pickle_df
+
+
+def concatenate_cleaned_pickles(list_of_pickle_df):
+    temp_ = pd.concat(clean_pickles(list_of_pickle_df)).reset_index(drop=True)
+    return temp_
+        
+     
+def X_y_split_simple(df):
+    X_ = df.drop(columns=['WhiteIsComp', 'BlackIsComp', 'NoComp'])
+    y_ = df.loc[:,['WhiteIsComp', 'BlackIsComp', 'NoComp']]
+    return X_, y_
+
+
+def y_convert_to_ohe_vec(y):
+    '''
+    Note: Loss will have to be 'categorical_crossentropy' if y's are one-hot-encoded, 'sparse_categorical_crossentropy' if y's are integers.
+    
+    If all games contain a cheater, return integers for y where:
+        0 indicates white was cheating
+        1 indicates black was cheating
+        
+    If any games contain no cheaters, return one-hot-encoded arrays for y where:
+        [1,0,0] indicates white was cheating
+        [0,1,0] indicates black was cheating
+        [0,0,1] indicates no-one was cheating
+        
+        
+    Have a check for explicit 'options=3' where user can input options value, or check len(y.columns)==3
+    '''
+    if len(y.columns)==3:
+        if all(y.NoComp==0):
+            y_ = pd.DataFrame(np.where(y.WhiteIsComp==1, 0, 1), columns=['0_WhiteIsComp_1_BlackIsComp'])
+            return y_
+        else:
+            y_ = pd.DataFrame([[row] for row in y.values], columns=['WhiteIsComp_BlackIsComp_NoComp'])
+            return y_        
+    else:
+        return(print('Function only currently supporting y having WhiteIsComp, BlackIsComp, and NoComp'))
